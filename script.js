5ced5068855266978941cba5cce8e9f427288589
Matheus Fernandes Cunha <matheus.cunha@ioc.fiocruz.br>
2026-01-11 16:35:28 -0300

    v2

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 fetch("dados.json")
-.then(response => response.json())
+.then(r => r.json())
 .then(dados => {
+
+    const headerRow = document.getElementById("header-row");
     const corpo = document.getElementById("tabela-corpo");
 
+    // cria colunas com áreas
     dados.forEach(item => {
-        corpo.innerHTML += `
-        <tr>
-            <td>${item.area}</td>
-            <td>${item.aplicacao}</td>
-            <td>${item.indicadores}</td>
-            <td>${item.tipo}</td>
-            <td>${item.impactos}</td>
-            <td>${item.maturidade}</td>
-        </tr>
-        `;
+        headerRow.innerHTML += `<th>${item.area}</th>`;
     });
+
+    const campos = [
+        { label: "Aplicação", key: "aplicacao" },
+        { label: "Indicadores", key: "indicadores" },
+        { label: "Tipo de risco", key: "tipo" },
+        { label: "Resultados e impactos", key: "impactos" },
+        { label: "Maturidade / Barreiras / Integração", key: "maturidade" }
+    ];
+
+    campos.forEach(campo => {
+        let linha = `<tr><th>${campo.label}</th>`;
+
+        dados.forEach(item => {
+            linha += `<td>${item[campo.key]}</td>`;
+        });
+
+        linha += `</tr>`;
+        corpo.innerHTML += linha;
+    });
+
 });
